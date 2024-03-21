2c343c79b0a4ef1fef2e980cf9b63a7d83d58e2a
yukiiga <yuki_0304@outlook.com>
2024-03-21 21:56:47 +0900

    view作成から表示まで

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,5 +13,17 @@ class PostController extends Controller
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
+    
+    /**
+     * 特定IDのpostを表示する
+     *
+     * @params Object Post // 引数の$postはid=1のPostインスタンス
+     * @return Reposnse post view
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
 }
 ?>
