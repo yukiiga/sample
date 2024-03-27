5d77a6d34c03e4645f772d2797e2e637d3cf0d72
yukiiga <yuki_0304@outlook.com>
2024-03-27 11:35:58 +0900

    ブログ投稿の削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,4 +41,10 @@ class PostController extends Controller
 
     return redirect('/posts/' . $post->id);
     }
+    
+    public function delete(Post $post)
+    {
+    $post->delete();
+    return redirect('/');
+    }
 }
